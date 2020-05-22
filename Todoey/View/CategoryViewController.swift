8f0dfd9c5b0486f051e2611ab8943ab4f3e824e1
mdave94 <molnardavid2002@gmail.com>
2020-05-22 20:55:11 +0200

    Swipable option added, but not finished.

--- Todoey/View/CategoryViewController.swift
+++ Todoey/View/CategoryViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import RealmSwift
-
+import SwipeCellKit
 
 class CategoryViewController: UITableViewController{
     
@@ -71,7 +71,7 @@ class CategoryViewController: UITableViewController{
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath) as! SwipeTableViewCell
         
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "NO Categories added"
@@ -119,4 +119,25 @@ class CategoryViewController: UITableViewController{
         
     }
 
+}
+
+//MARK:- SwipeTableCellDelegate methods
+
+extension CategoryViewController: SwipeTableViewCellDelegate{
+        
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title :"Delete"){ action, indexpath in
+            // Handle action
+            
+            print("deleted item")
+        }
+        
+        deleteAction.image = UIImage(named:"delete-icon")
+        
+       return [deleteAction]
+    } 
+    
 }
